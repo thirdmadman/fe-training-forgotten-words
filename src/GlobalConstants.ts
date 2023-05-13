62159321feeba0e3555a289125f917f02cb0d4e9
thirdmadman <thirdmadman@gmail.com>
2023-05-13 18:47:59 +0200

    feat: add basic services

--- src/GlobalConstants.ts
+++ src/GlobalConstants.ts
@@ -1,7 +1,7 @@
 export class GlobalConstants {
   public static APP_NAME = 'Forgotten Words';
 
-  public static DEFAULT_API_URL = process.env.NODE_ENV === 'development' ? process.env.DEFAULT_API_URL : '/api';
+  public static DEFAULT_API_URL = process.env.NODE_ENV === 'development' ? process.env.DEFAULT_API_URL || '' : '/api';
 
   public static ROUTE_MAIN = '/';
 
