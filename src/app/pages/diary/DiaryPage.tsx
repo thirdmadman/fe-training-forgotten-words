dc3ec9f104893f6030ad599a04c7ac0050748de2
thirdmadman <thirdmadman@gmail.com>
2023-05-23 00:07:17 +0200

    feat: impl use of Pagination component feat: impl use of page params

--- src/app/pages/diary/DiaryPage.tsx
+++ src/app/pages/diary/DiaryPage.tsx
@@ -1,23 +1,63 @@
 import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { GlobalConstants } from '../../../GlobalConstants';
+import { Pagination } from '../../components/common/pagination/Pagination';
+import { DiaryCardsContainer } from '../../components/diary/DiaryCardsContainer';
 import { IAggregatedWord } from '../../interfaces/IAggregatedWord';
 import { IPaginatedArray } from '../../interfaces/IPaginatedArray';
 import { TokenProvider } from '../../services/TokenProvider';
 import { UsersAggregatedWordsService } from '../../services/UsersAggregatedWordsService';
+import './diaryPage.scss';
 
 export function DiaryPage() {
+  const WORDS_PER_DIARY_PAGE = 50;
+  const navigate = useNavigate();
+  const params = useParams();
   const [userDiaryWords, setUserDiaryWords] = useState<IPaginatedArray<IAggregatedWord> | null>(null);
 
+  const pageNumber = userDiaryWords ? userDiaryWords.currentPage : 0;
+  const totalPages = userDiaryWords ? Math.floor(userDiaryWords.size / WORDS_PER_DIARY_PAGE) : 0;
+
+  const page = Number(params.page ? params.page : '1') - 1;
+
+  const changePage = (direction: string) => {
+    if (direction === 'prev') {
+      if (page > 0) {
+        navigate(`${GlobalConstants.ROUTE_DIARY}/${pageNumber}`);
+      }
+    }
+
+    if (direction === 'next') {
+      if (page < totalPages) {
+        navigate(`${GlobalConstants.ROUTE_DIARY}/${pageNumber + 2}`);
+      }
+    }
+  };
+
   useEffect(() => {
-    console.error('useEffect');
+    if (!params.page) {
+      navigate(`${GlobalConstants.ROUTE_DIARY}/1`);
+      return;
+    }
+
     const userId = TokenProvider.getUserId();
     if (userId && !TokenProvider.checkIsExpired()) {
-      UsersAggregatedWordsService.getAggregatedWordsWithResults(userId, 0, 100)
+      UsersAggregatedWordsService.getAggregatedWordsWithResults(userId, page, WORDS_PER_DIARY_PAGE)
         .then((userAggregatedWords) => setUserDiaryWords(userAggregatedWords))
         .catch((e) => console.error(e));
     }
-  }, [setUserDiaryWords]);
+  }, [setUserDiaryWords, page, navigate, params]);
 
-  console.error(userDiaryWords);
-
-  return <div className="diary-page">diary</div>;
+  return (
+    <div className="diary">
+      <div className="navigation-container">
+        <Pagination
+          text={`Page number ${pageNumber + 1}/${totalPages + 1}`}
+          onPreviousAction={() => changePage('prev')}
+          onNextAction={() => changePage('next')}
+        />
+      </div>
+      {userDiaryWords && <DiaryCardsContainer wordsPaginatedArray={userDiaryWords} />}
+    </div>
+  );
 }
