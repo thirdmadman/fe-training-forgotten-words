317a0de485e204692e3ad91e6cb76cd14c20cdcb
thirdmadman <thirdmadman@gmail.com>
2023-06-03 16:33:22 +0200

    feat: impl pause in timer when menu is open

--- src/app/components/sprint/SprintTimer.tsx
+++ src/app/components/sprint/SprintTimer.tsx
@@ -10,12 +10,13 @@ interface SprintTimerProps {
 export function SprintTimer(props: SprintTimerProps) {
   const { timerTime, timerOnFinishAction } = props;
   const timerRemainTime = useAppSelector((state: RootState) => state.timer.timerRemainTime);
+  const isMenuHidden = useAppSelector((state: RootState) => state.menu.isHidden);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (timerRemainTime === -1) {
       dispatch(setRemainTime(timerTime));
-    } else if (timerRemainTime > 0) {
+    } else if (timerRemainTime > 0 && isMenuHidden) {
       window.setTimeout(() => dispatch(setRemainTime(timerRemainTime - 1)), 1000);
     } else {
       timerOnFinishAction();
