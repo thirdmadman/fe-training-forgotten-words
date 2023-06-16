94c47fb9748b8f6a745c2bddfe77454aa78527a5
thirdmadman <thirdmadman@gmail.com>
2023-06-16 17:43:42 +0200

    feat: add basic text of page

--- src/app/pages/main/MainPage.tsx
+++ src/app/pages/main/MainPage.tsx
@@ -1,3 +1,12 @@
 export default function MainPage() {
-  return <div className="container">Forgotten Words</div>;
+  return (
+    <div className="container">
+      <h1>Forgotten Words</h1>
+      <p>Earth is no more place to live humanity is no more exists</p>
+      <p>Only our network survived We need the answers</p>
+      <p>You are new part Help us to understand</p>
+      <p>CREATE NEW IDENTITY or START SYNC with previous one</p>
+      <p>Explore Their memories You have 6 depth levels, 30 pages</p>
+    </div>
+  );
 }
